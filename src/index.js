43e39f4bdf3bb05b76cc910484aea1d9a989de00
Gal Somovic <galsomovic@gmail.com>
2022-10-04 18:32:04 +0200

    added login page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, onValue, update } from "firebase/database";
+import { getAuth } from "firebase/auth";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +17,58 @@ const firebaseConfig = {
     storageBucket: "expensify-86198.appspot.com",
     messagingSenderId: "505683998272",
     appId: "1:505683998272:web:dff210d4cbd2b4b5e291a5",
+    databaseURL: "https://expensify-86198-default-rtdb.europe-west1.firebasedatabase.app/",
     measurementId: "G-2W4B0577SV"
 };
-
+export { auth, db , firebase,firebaseui};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getDatabase();
+const auth = getAuth(app);
+
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
+
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+if (ui.isPendingRedirect()) {
+    ui.start('#firebaseui-auth-container', {
+        signInOptions: [
+            {
+                provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+            }
+        ],
+    });
+}
+
+var uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+        },
+        uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+        }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
